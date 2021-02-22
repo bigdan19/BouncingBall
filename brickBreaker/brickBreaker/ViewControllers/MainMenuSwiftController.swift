1ee557ea7cb19ecc883ee113efd89cdc514eb307
Daniel <bogucki19@gmail.com>
2021-02-22 21:47:44 +0000

    Fixed Buttons in main menu

--- brickBreaker/brickBreaker/ViewControllers/MainMenuSwiftController.swift
+++ brickBreaker/brickBreaker/ViewControllers/MainMenuSwiftController.swift
@@ -12,20 +12,14 @@ class MainMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBOutlet weak var PlayNowIcon: UIButton!
-    @IBOutlet weak var SettingsIcon: UIButton!
-    @IBOutlet weak var AboutIcon: UIButton!
     
     @IBAction func PlayNowButtonPressed(_ sender: Any) {
-        PlayNowIcon.setImage(UIImage(named: "PlayNowYellowIcon"), for: .normal)
     }
     
     @IBAction func SettingsButtonPressed(_ sender: Any) {
-        SettingsIcon.setImage(UIImage(named: "SettingsYellowIcon"), for: .normal)
     }
     
     @IBAction func AboutButtonPressed(_ sender: Any) {
-        AboutIcon.setImage(UIImage(named: "AboutYellowIcon"), for: .normal)
     }
     
     
