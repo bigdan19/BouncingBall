6c945d067999d84f1e694f353669cc30909f4275
Daniel <bogucki19@gmail.com>
2021-02-22 21:37:36 +0000

    About page was created

---
+++ brickBreaker/brickBreaker/ViewControllers/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  brickBreaker
+//
+//  Created by Daniel on 22/02/2021.
+//  Copyright © 2021 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func GoBackButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
