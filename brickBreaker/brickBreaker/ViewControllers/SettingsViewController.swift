2d97a175992b37185ce5b5cf99fee22cebb003a3
Daniel <bogucki19@gmail.com>
2021-03-08 20:46:35 +0000

    Added empty seting viewcontroller

---
+++ brickBreaker/brickBreaker/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  brickBreaker
+//
+//  Created by Daniel on 08/03/2021.
+//  Copyright © 2021 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func goBackButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
